da4dd282a8d43e1dd35e42994724421142c0ff23
Elanum <mail@elanum.de>
2020-12-23 18:33:47 +0100

    fix: remove Container from logout view

--- client/src/components/auth/logout.component.js
+++ client/src/components/auth/logout.component.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Container } from 'react-bootstrap';
 import * as actions from '../../actions';
 
 class Logout extends Component {
