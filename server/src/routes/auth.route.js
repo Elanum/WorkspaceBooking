8c90b088feb06756bd45811a5fe076fb778d03a6
Elanum <mail@elanum.de>
2021-01-01 21:55:31 +0100

    revert: remove change-password route

--- server/src/routes/auth.route.js
+++ server/src/routes/auth.route.js
@@ -1,8 +1,7 @@
 import express from 'express';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
-import passport from 'passport';
-import User from '../models/users.model';
+import Users from '../models/users.model';
 
 const router = express.Router();
 const secret = process.env.SECRET || 'jwtsecret';
@@ -12,7 +11,7 @@ router
   .route('/login')
   .post(async (req, res) => {
     const { username, password } = req.body;
-    const user = await User.findOne({ username }).select('+password');
+    const user = await Users.findOne({ username }).select('+password');
     if (!username) {
       return res.status(400).json({ message: 'Please enter a username' });
     }
@@ -46,7 +45,7 @@ router
 router
   .route('/register')
   .post(async (req, res) => {
-    const user = new User({ ...req.body });
+    const user = new Users({ ...req.body });
     try {
       await user.save();
     } catch (error) {
@@ -55,43 +54,4 @@ router
     return res.status(201).json(user);
   })
   .all((_req, res) => res.status(405).json({ message: 'Method Not Allowed' }));
-
-router
-  .route('/change-password')
-  .put(
-    passport.authenticate('jwt', { session: false, failWithError: true }),
-    async (req, res) => {
-      const { username, oldPassword, newPassword } = req.body;
-
-      if (!username) {
-        return res.status(400).json({ message: 'Username is missing' });
-      }
-      if (!oldPassword) {
-        return res.status(400).json({ message: 'Old Password is missing' });
-      }
-      if (!newPassword) {
-        return res.status(400).json({ message: 'New Password is missing' });
-      }
-
-      const user = await User.findOne({ username }).select('+password');
-
-      if (!user) return res.status(404).json({ message: 'User not found' });
-      const passwordMatch = await bcrypt.compare(oldPassword, user.password);
-
-      if (!passwordMatch) {
-        return res.status(400).json({ message: 'Old password incorrect' });
-      }
-
-      user.password = newPassword;
-      try {
-        await user.save();
-      } catch (error) {
-        return res.status(500).json({ message: error.message });
-      }
-      return res.status(200).json({ message: 'Password updated' });
-    },
-    (error, _req, res, _next) => res.status(error.status || 500).json({ message: error.message }),
-  )
-  .all((_req, res) => res.status(405).json({ message: 'Method Not Allowed' }));
-
 module.exports = router;
