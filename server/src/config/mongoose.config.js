85b8f8713441d9f826d923435e916c19a8c64b24
Elanum <mail@elanum.de>
2021-02-05 14:34:05 +0100

    refactor: create a full mongoose init script

--- server/src/config/mongoose.config.js
+++ server/src/config/mongoose.config.js
@@ -4,31 +4,47 @@ import Rooms from '../models/rooms.model';
 import Workspaces from '../models/workspaces.model';
 
 const {
+  DB_URI,
+  NODE_ENV,
   ADMIN_USER = 'admin',
   ADMIN_PWD = 'rootpwd',
   ADMIN_MAIL = 'admin@workspace-booking.de',
 } = process.env;
 
 const users = [
-  {
-    username: ADMIN_USER,
-    password: ADMIN_PWD,
-    email: ADMIN_MAIL,
-  },
+  { username: ADMIN_USER, password: ADMIN_PWD, email: ADMIN_MAIL },
 ];
 
 const rooms = [
-  {
-    _id: mongoose.mongo.ObjectID(),
-    name: 'Room A',
-  },
-  {
-    _id: mongoose.mongo.ObjectID(),
-    name: 'Room B',
-  },
+  { _id: mongoose.mongo.ObjectID(), name: 'Room A' },
+  { _id: mongoose.mongo.ObjectID(), name: 'Room B' },
 ];
 
-const addUsers = () => {
+const dbConnection = NODE_ENV === 'production' || NODE_ENV === 'staging'
+  ? DB_URI
+  : 'mongodb://localhost:27017/';
+
+const dbName = NODE_ENV === 'production' ? 'workspace-booking' : 'workspace-booking-dev';
+
+const connect = async () => {
+  if (mongoose.connection.readyState === 0) {
+    await mongoose.connect(dbConnection, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      dbName,
+    });
+  }
+};
+
+const disconnect = (done) => {
+  if (mongoose.connection.readyState !== 0) {
+    mongoose.disconnect(done);
+  }
+};
+
+const initUsers = () => {
   Users.findOne().then(async (u) => {
     if (!u) {
       await Users.create(users, (error) => {
@@ -38,7 +54,7 @@ const addUsers = () => {
   });
 };
 
-const addRooms = () => {
+const initRooms = () => {
   Rooms.findOne().then(async (r) => {
     if (!r) {
       await Rooms.create(rooms, (error) => {
@@ -48,7 +64,7 @@ const addRooms = () => {
   });
 };
 
-const addWorkspaces = () => {
+const initWorkspaces = () => {
   const workspaces = [];
   Workspaces.find().then(async (w) => {
     if (w.length < 1) {
@@ -67,6 +83,15 @@ const addWorkspaces = () => {
   });
 };
 
-addUsers();
-addRooms();
-addWorkspaces();
+const initData = () => {
+  initUsers();
+  initRooms();
+  initWorkspaces();
+};
+
+export default {
+  mongoose,
+  connect,
+  disconnect,
+  initData,
+};
