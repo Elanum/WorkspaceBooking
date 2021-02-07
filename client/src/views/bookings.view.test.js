961656cd5f9abcf4d437d10dd9ddfd2fec2ef973
Manuel Schierenberg <mail@elanum.de>
2021-02-07 16:00:29 +0100

    test: add simple tests for all views

--- client/src/views/bookings.view.test.js
+++ client/src/views/bookings.view.test.js
@@ -3,24 +3,26 @@ import Adapter from 'enzyme-adapter-react-16';
 import React from 'react';
 import { Provider } from 'react-redux';
 import configureStore from 'redux-mock-store';
-import Login from './login.view';
+import thunk from 'redux-thunk';
+import Bookings from './bookings.view';
 
 configure({ adapter: new Adapter(), disableLifecycleMethods: true });
 
 let wrapper;
-const mockStore = configureStore([]);
-const store = mockStore({ auth: { errorMessage: '' } });
+const middlewares = [thunk];
+const mockStore = configureStore(middlewares);
+const store = mockStore({ bookings: { bookings: [] } });
 
 describe('Login View', () => {
   beforeEach(() => {
     wrapper = mount(
       <Provider store={store}>
-        <Login />
+        <Bookings />
       </Provider>,
     );
   });
 
-  it('should have form', () => {
-    expect(wrapper.find('form').exists()).toBeTruthy();
+  it('should have Table', () => {
+    expect(wrapper.find('table').exists()).toBeTruthy();
   });
 });
