2c91365ffc65dfb7566262b5e8174fb03af1703a
Manuel Schierenberg <mail@elanum.de>
2021-02-07 16:05:33 +0100

    feat: remove room views

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,5 +1,4 @@
 export { login, logout } from './auth.actions';
 export { getUser } from './users.actions';
-export { getRooms } from './rooms.actions';
 export { getBookings, postBookings } from './bookings.actions';
 export { getWorkspaces } from './workspaces.actions';
